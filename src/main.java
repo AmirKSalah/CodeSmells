066478f6a9395b3acf78f6611bb26fc7298d054f
sqz <36668060+sqz269@users.noreply.github.com>
2023-11-17 18:46:54 -0500

    Update main.java

--- src/main.java
+++ src/main.java
@@ -3,10 +3,17 @@ import java.util.List;
 
 public class main {
 	public static void main(String[] args) {
-        Item item1 = new Item("Book", 20, 1, DiscountType.AMOUNT, 5);
-        Item item2 = new TaxableItem("Laptop", 1000, 1, DiscountType.PERCENTAGE, 0.1);
-        Item item3 = new GiftCardItem("Gift Card", 10, 1, DiscountType.AMOUNT, 0);
-
+	Item item1 = new Item("Book", 20, 1);
+	item1.addDiscount(DiscountType.AMOUNT, 5);
+	
+	// Creating item2 (a TaxableItem)
+	TaxableItem item2 = new TaxableItem("Laptop", 1000, 1);
+	item2.addDiscount(DiscountType.PERCENTAGE, 0.1);
+	
+	// Assuming GiftCardItem is similar to TaxableItem but without tax functionalities
+	GiftCardItem item3 = new GiftCardItem("Gift Card", 10, 1);
+	item3.addDiscount(DiscountType.AMOUNT, 0);
+		
         List<Item> items = new ArrayList<>();
         items.add(item1);
         items.add(item2);
