35eb71168c643131b40ee459dc78df6712149489
sqz <36668060+sqz269@users.noreply.github.com>
2023-11-17 18:45:11 -0500

    Update TaxableItem.java

--- src/TaxableItem.java
+++ src/TaxableItem.java
@@ -1,16 +1,16 @@
-
 public class TaxableItem extends Item {
     private double taxRate = 7;
-    
-    public TaxableItem(String name, double price, int quantity, DiscountType discountType, double discountAmount){
-        super(name, price, quantity, discountType, discountAmount);
+
+    public TaxableItem(String name, double price, int quantity) {
+        super(name, price, quantity);
     }
 
-    public double getTaxRate(){
+    public double getTaxRate() {
         return taxRate;
     }
+
     public void setTaxRate(double rate) {
-        if(rate>=0){
+        if (rate >= 0) {
             taxRate = rate;
         }
     }
